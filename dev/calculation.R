a552c186c74069faab3ee83de3b55a960a87f7b8
Kate O'Hara <63014805+oharakate@users.noreply.github.com>
2025-12-03 10:38:01 +0000

    Loaded data and labels for NCAI calc.

---
+++ dev/calculation.R
@@ -0,0 +1,74 @@
+## DEVELOP CALCULATION PROCESS FOR openNCAI
+
+# Things in use
+# VECTOR of EA Ecosystem Area
+# gets multiplied by
+# ESPU Ecosystem service potential per unit/hectare
+# to make
+# MATRIX ESPB Ecosystem service potential base
+
+# ESPB cell proportion of column
+# gets multiplied by
+# VECTOR ESPW Ecosystem service potential weighting
+# to give
+# WBB Wellbeing base
+
+#
+
+
+# Data
+
+wb   <- read.csv("dev/wellbeing_base.csv", header = FALSE)
+espb <- read.csv("dev/ecosystem_potential_base.csv", header = FALSE)
+espu <- read.csv("dev/ecosystem_potential_per_service_provisioning_unit.csv", header = FALSE)
+
+# Labels for data:
+
+main_labels <- c("provisioning", "regulation_and_maintenance", "cultural")
+main_weights <- c(10,20,10)
+provisioning_weights <- c(20,15,9,9,20,13,13,7,11,12,1,0)
+provisioning_labels <- c("cultivated_crops",
+                         "reared_animals",
+                         "wild_animals_plants_algae",
+                         "aquaculture_animals_plants_algae",
+                         "water_drinking",
+                         "materials_direct_animals_plants_algae",
+                         "materials_agricultural_animals_plants_algae",
+                         "genetic_material",
+                         "water_non-drinking",
+                         "plant_energy",
+                         "animal_energy",
+                         "animal_mechanical_energy")
+regulationandmaintenance_weights <- c(10,10,12,7,7,10,10,12,14,8,20)
+regulationandmaintenance_labels <- c("waste_mediation_biota",
+                                     "waste_mediation_ecosystem",
+                                     "erosion_mediation",
+                                     "flood_protection",
+                                     "storm_protection",
+                                     "pollination_dispersal",
+                                     "nursery_population_habitat",
+                                     "pest_disease_control",
+                                     "soil_formation_composition",
+                                     "water_chemistry",
+                                     "climate")
+cultural_weights <- c(20,20,20,20,20)
+cultural_labels <- c("physical_experience",
+                     "heritage_educational",
+                     "aesthetic_entertainment",
+                     "symbolic_sacred_religious",
+                     "existence_bequest")
+spu_labels <- c("b1", "b2", "b3", "c", "d1", "d2", "d4", "d5",
+                "e1", "e2", "e4","e5", "e7",
+                "f2","f3","f4","f9",
+                "g1","g3","g4","g5","g6",
+                "h2", "h3","i1","i2",
+                "j1","j2","j3","j4","k")
+
+# Apply labels
+
+colnames(espb) <- colnames(espu) <- colnames(wb) <-
+  c(provisioning_labels,regulationandmaintenance_labels,cultural_labels)
+rownames(espb) <- rownames(espu) <-rownames(wb) <- spu_labels
+
+
+lapply(names(wb))
